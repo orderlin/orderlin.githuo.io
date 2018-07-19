be6fa3d1d08cc09e532bfaa9cfc25afeb59fa270
feng.hailin <763435946@qq.com>
2018-07-19 20:15:26 +0800

    access

--- index.php
+++ index.php
@@ -1,7 +1,29 @@
 <?php
 define("TOKEN", "linerShow");
-$wechatObj = new wechatCallbackapiTest();
-$wechatObj->getRes();
+
+
+if(isset($_GET)){
+    //验证服务器
+    $nonce     = $_GET['nonce'];
+    $timestamp = $_GET['timestamp'];
+    $echostr   = $_GET['echostr'];
+    $signature = $_GET['signature'];
+    //形成数组，然后按字典序排序
+    $array = array();
+    $array = array($nonce, $timestamp, TOKEN);
+    sort($array);
+    //拼接成字符串,sha1加密 ，然后与signature进行校验
+    $str = sha1( implode( $array ) );
+    if( $str == $signature && $echostr ){
+        //第一次接入weixin api接口的时候
+        echo  $echostr;
+        exit;
+    }
+}else{
+    $wechatObj = new wechatCallbackapiTest();
+    $wechatObj->getRes();
+}
+
 class wechatCallbackapiTest
 {
     //    function responseMsg()
