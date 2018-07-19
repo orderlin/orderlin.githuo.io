c9b5e62095e499c6d5e9f805d7d60966e33ea381
feng.hailin <763435946@qq.com>
2018-07-19 21:31:22 +0800

    test

--- index.php
+++ index.php
@@ -1,31 +1,42 @@
 <?php
 define("TOKEN", "linerShow");
 
+
+// 验证服务器
+$nonce = isset($_GET['nonce']) ? $_GET['nonce'] : '';
+$timestamp = isset($_GET['timestamp']) ? $_GET['timestamp'] : '';
+$echostr = isset($_GET['echostr']) ? $_GET['echostr'] : '';
+$signature = isset($_GET['signature']) ? $_GET['signature'] : '';
+// 形成数组，然后按字典序排序
+$array = array();
+$array = array(
+    $nonce,
+    $timestamp,
+    TOKEN
+);
+sort($array);
+// 拼接成字符串,sha1加密 ，然后与signature进行校验
+$str = sha1(implode($array));
+/*
+ * 第一次接入验证
+ */
 if (isset($_GET['echostr'])) {
-    // 验证服务器
-    $nonce = $_GET['nonce'];
-    $timestamp = $_GET['timestamp'];
-    $echostr = $_GET['echostr'];
-    $signature = $_GET['signature'];
-    // 形成数组，然后按字典序排序
-    $array = array();
-    $array = array(
-        $nonce,
-        $timestamp,
-        TOKEN
-    );
-    sort($array);
-    // 拼接成字符串,sha1加密 ，然后与signature进行校验
-    $str = sha1(implode($array));
     if ($str == $signature && $echostr) {
         // 第一次接入weixin api接口的时候
         echo $echostr;
         exit();
     }
-} else {
+} 
+/*
+ * 后续接入验证
+ */
+elseif(isset($_GET['$signature']){
+    if ($str == $signature）{
+        return true;
+    }
+}else {
     $postArray = $_REQUEST;
     file_put_contents('/tmp/test.txt', json_encode($postArray));
 }
 
 
-?>
