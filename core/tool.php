d20d0cdbe7d8d291630d1f9b22002510d8fb7630
feng.hailin <763435946@qq.com>
2018-09-12 21:43:37 +0800

    test

--- core/tool.php
+++ core/tool.php
@@ -9,7 +9,7 @@ class tool{
         //return $content;
         if(preg_match("#.*?item.taobao.com.*?id=(\d+)#isu", $content, $info)){
             $tb_item_id = $info[1];
-            return '宝贝加入成功,回复\<曲线图\>查看价格曲线图,2天后才会出图';
+            return '宝贝加入成功';
         }else{
             return '1';
         }
