490a4d7ab7e1510e27f9b82d8c7afa651668641a
feng.hailin <763435946@qq.com>
2018-10-16 22:01:36 +0800

    test

--- model/muscle.php
+++ model/muscle.php
@@ -1,5 +1,6 @@
 <?php
 namespace linerModel;
+use \PDO as PDO;
 
 class muscle{
     
@@ -31,12 +32,12 @@ class muscle{
             
             $this->dsn = 'mysql:host='.$dbHost.';dbname='.$dbName;
             
-            $this->dbh = new \PDO($this->dsn, $dbUser, $dbPasswd);
+            $this->dbh = new PDO($this->dsn, $dbUser, $dbPasswd);
             file_put_contents('/tmp/db2.log', json_encode($this->dsn));
             file_put_contents('/tmp/db3.log', json_encode($this->dbh));
             $this->dbh->exec('SET character_set_connection='.$dbCharset.', character_set_results='.$dbCharset.', character_set_client=binary');
             
-        } catch (\PDOException $e) {
+        } catch (PDOException $e) {
             
             $this->outputError($e->getMessage());
             file_put_contents('/tmp/db4.log', json_encode($this->outputError($e->getMessage())));
