fea64aae91ff3edfe9b10978954439aba2cc54b1
feng.hailin <763435946@qq.com>
2018-10-18 21:58:24 +0800

    test

--- controller/checkItem.php
+++ controller/checkItem.php
@@ -3,7 +3,8 @@ namespace linerController;
 use linerModel\muscle;
 class checkItem{
     
-    static function insertCheckItem($db, $folder_id, $b2c, $b2c_id){
+    static function insertCheckItem($folder_id, $b2c, $b2c_id){
+        $db = muscle::getInstance()->dbh;
         $sql = "select count(*) from item where folder_id = {$folder_id} and b2c = {$b2c} and b2c_id = '{$b2c_id}'";
         $count = $db->query($sql)->fetchColumn();
         if($count == 0){
